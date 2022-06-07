fce3b051647e569903212e9dfa6894dac3de05bd
rmiskiewicz <rmiskiewicz@core-logic.com>
2022-06-07 23:06:28 +0200

    init add player

--- app/src/main/java/pl/rafalmiskiewicz/mafia/ui/playerList/PlayerListViewModel.kt
+++ app/src/main/java/pl/rafalmiskiewicz/mafia/ui/playerList/PlayerListViewModel.kt
@@ -7,11 +7,33 @@ import pl.rafalmiskiewicz.mafia.ui.base.BaseViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class PlayerListViewModel@Inject constructor(): BaseViewModel<PlayerListEvent>() {
+class PlayerListViewModel @Inject constructor() : BaseViewModel<PlayerListEvent>() {
 
     val playerList = MutableLiveData<List<PlayerItem>>()
 
     fun onProfileClicked(id: Int) {
         playerList.value = playerList.value?.filter { it.id != id }
+    }
+
+    fun onAddClicked() {
+
+        val newPlayerList = playerList.value?.toMutableList()
+        newPlayerList?.add(
+            PlayerItem(
+                id = newPlayerList.maxOf { it.id } + 1,
+                name = " Marzenka Peperka"
+            )
+        )
+        newPlayerList.let {
+            playerList.value = it
+            if (it != null) {
+                it += PlayerItem(
+                    id = 1,
+                    name = " Marzenka Peperka"
+                )
+            }
+        }
+
+
     }
 }
